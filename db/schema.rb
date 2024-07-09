30dd8c7527a8def32d3840d6b2516f0241373db9
christian carbonari <christiancarbonari9@gmail.com>
2024-07-09 20:37:15 +1000

    auth features added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_053901) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.boolean "Billionaire"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "yachts", force: :cascade do |t|
