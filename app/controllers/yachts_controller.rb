bb9a90ddb1574313ae91a9f24aa59bc0593b3633
pamera timu <pamerapaasi@gmail.com>
2024-07-11 12:04:35 +0200

    added the edit and update function in yachts controller

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -4,9 +4,9 @@ class YachtsController < ApplicationController
     @yachts = Yacht.all
   end
 
-  # def show
-  #   @yacht = Yacht.find(params[:id])
-  # end
+   def show
+     @yacht = Yacht.find(params[:id])
+   end
 
   def new
     @yacht = Yacht.new
@@ -21,6 +21,19 @@ class YachtsController < ApplicationController
     end
   end
 
+  def edit
+    @yacht = Yacht.find(params[:id])
+  end
+
+   def update
+    if yacht.update(yacht_params)
+      redirect_to yacht_path(@yacht)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+   end
+   
+  #
   # def destroy
   #   @yacht.destroy
   #   redirect_to yachts_path, status: :see_other
