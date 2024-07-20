47f954f1d2a2de0e2f4d0997ee01fd7eaa41e644
pamera timu <pamerapaasi@gmail.com>
2024-07-20 05:41:54 +0200

    changed navbar welcome to first_name

--- app/controllers/user/yachts_controller.rb
+++ app/controllers/user/yachts_controller.rb
@@ -2,4 +2,16 @@ class User::YachtsController < ApplicationController
   def index
     @yachts = current_user.yachts
   end
+
+  def edit
+  end
+
+  def update
+    if @yacht.update(yacht_params)
+      redirect_to @yacht, notice: 'Yacht was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
 end
