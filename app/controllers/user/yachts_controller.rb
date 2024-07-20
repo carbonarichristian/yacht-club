444b639439c88e7641d1df8f6cac037a9012bf9b
pamera timu <pamerapaasi@gmail.com>
2024-07-20 06:05:18 +0200

    made show page more dynamic so that it hides the dit and delete function from unauthorized users

--- app/controllers/user/yachts_controller.rb
+++ app/controllers/user/yachts_controller.rb
@@ -3,15 +3,4 @@ class User::YachtsController < ApplicationController
     @yachts = current_user.yachts
   end
 
-  def edit
-  end
-
-  def update
-    if @yacht.update(yacht_params)
-      redirect_to @yacht, notice: 'Yacht was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
 end
